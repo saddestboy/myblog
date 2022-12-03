00bad39df63e7850fc972e59d8ab73f3d2f2d365
saddestboy <descent@saddestboy.anonaddy.me>
2022-12-03 14:50:44 +0800

    initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+// docs/.vuepress/config.js
+module.exports = {
+  title: "学习笔记",
+  description: "一点学习过程中的笔记，随便看看XD",
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    sidebar: [
+      {
+        title: '学习笔记',
+        children: ['/mylearning/目录' ],
+        initialOpenGroupIndex: 1 // 可选的, 默认值是 0
+      }
+    ]
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroText: 一个随便写写随便发发的地方
+tagline: 公众号：暂无
+actionText: 查看文章 →
+actionLink: /mylearning/目录
+features:
+- title: 学习笔记
+  details: 一点学习过程中的笔记，随便看看XD
+- title: 游戏感受
+  details: 主要分享一些游玩游戏过程中体验，分享游玩结束截图纪念^v^
+footer: 学习笔记 | 游玩体验
+---
